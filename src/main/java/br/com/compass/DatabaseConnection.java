51e97480f26d64f5af4f8dd26187bd483b02f375
Jessica <jeessicats@gmail.com>
2025-01-22 14:01:50 -0300

    Initial commit

---
+++ src/main/java/br/com/compass/DatabaseConnection.java
@@ -0,0 +1,23 @@
+package br.com.compass;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/bank_challenge";
+    private static final String USER = "bank-challenge";
+    private static final String PASSWORD = "B@nkChallenge";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("JDBC Driver not found.");
+            e.printStackTrace();
+        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
+
+
